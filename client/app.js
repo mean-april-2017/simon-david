ca1f359b872907829e2eaf6a6ffe113f9d0520d6
David Wolgemuth <davidjwolgemuth@gmail.com>
2017-04-11 14:46:57 -0500

    Inserted Comments

--- client/app.js
+++ client/app.js
@@ -16,13 +16,17 @@ app.controller("simonController", function ($scope) {
         $scope.lastClicked = { background: color };
 
         if ($scope.index === $scope.clicks.length) {
+            // Finished the pattern .. Add new color and change turn
             $scope.clicks.push(color);
             turnChange();
         } else {
+            // Continuing Pattern
             if ($scope.clicks[$scope.index] !== color) {
+                // Player Lost
                 alert("Incorrect Click!");
                 reset();
             } else {
+                // Okay Click
                 $scope.index += 1;
             }
         }
@@ -31,11 +35,13 @@ app.controller("simonController", function ($scope) {
 
     function turnChange () {
         alert("Switch Players");
+        // Toggle Players
         if ($scope.player === 0) {
             $scope.player = 1;
         } else {
             $scope.player = 2;
         }
+        // Set back to beginning of pattern
         $scope.index = 0;
     }
 });
