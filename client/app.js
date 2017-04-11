1bd163a68340707960687adb9d88274ee9d00c8d
David Wolgemuth <davidjwolgemuth@gmail.com>
2017-04-11 14:44:35 -0500

    finished challenge 2

--- client/app.js
+++ client/app.js
@@ -1,15 +1,41 @@
 
 var app = angular.module("simonApp", []);
 app.controller("simonController", function ($scope) {
-    $scope.player = 1;
-    $scope.lastClicked = null;
+
+    function reset () {
+        $scope.player = 1;
+        $scope.index = 0;
+        $scope.clicks = [];
+        $scope.lastClicked = null;
+    }
+
+    reset();
 
     $scope.clicked = function (color) {
+
         $scope.lastClicked = { background: color };
+
+        if ($scope.index === $scope.clicks.length) {
+            $scope.clicks.push(color);
+            turnChange();
+        } else {
+            if ($scope.clicks[$scope.index] !== color) {
+                alert("Incorrect Click!");
+                reset();
+            } else {
+                $scope.index += 1;
+            }
+        }
+
+    }
+
+    function turnChange () {
+        alert("Switch Players");
         if ($scope.player === 0) {
             $scope.player = 1;
         } else {
             $scope.player = 2;
         }
+        $scope.index = 0;
     }
 });
