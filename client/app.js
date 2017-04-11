1bac0dd8c1e753de0b2f399f529f45b0bc9869d2
David Wolgemuth <davidjwolgemuth@gmail.com>
2017-04-11 14:30:54 -0500

    finished challenge 1

--- client/app.js
+++ client/app.js
@@ -1,3 +1,15 @@
 
 var app = angular.module("simonApp", []);
-alert("Welcome To Simon!");
+app.controller("simonController", function ($scope) {
+    $scope.player = 1;
+    $scope.lastClicked = null;
+
+    $scope.clicked = function (color) {
+        $scope.lastClicked = { background: color };
+        if ($scope.player === 0) {
+            $scope.player = 1;
+        } else {
+            $scope.player = 2;
+        }
+    }
+});
